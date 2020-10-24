f559d2136a454ddaaa38e6b6c3f165dddf213507
Dietrich Epp <depp@zdome.net>
2020-10-24 15:12:57 -0400

    Create basic Nintendo 64 ROM image
    This just displays a changing solid color on the screen which cycles
    through the colors of the rainbow. This is a very basic program, which
    does not even use the RSP or RDP.

---
+++ game/main.c
@@ -0,0 +1,168 @@
+#include <ultra64.h>
+
+#define ARRAY_COUNT(x) (sizeof((x)) / sizeof((x)[0]))
+
+// Get a pointer to the start (top) of a stack.
+#define STACK_START(x) ((x) + sizeof((x)))
+
+enum {
+    // Size of stacks for threads, in bytes.
+    STACK_SIZE = 8 * 1024,
+
+    // Maximum number of simultaneous PI requests.
+    PI_MSG_COUNT = 8,
+
+    // Size of the framebuffer, in pixels.
+    SCREEN_WIDTH = 320,
+    SCREEN_HEIGHT = 240,
+};
+
+// Extern, because it is used by the boot code.
+u8 boot_stack[STACK_SIZE] __attribute__((aligned(8)));
+
+// Handle to access ROM data, from osCartRomInit.
+static OSPiHandle *rom_handle;
+
+static OSThread idle_thread;
+static u8 idle_thread_stack[STACK_SIZE] __attribute__((aligned(8)));
+
+static OSThread main_thread;
+static u8 main_thread_stack[STACK_SIZE] __attribute__((aligned(8)));
+
+static OSMesg pi_message_buffer[PI_MSG_COUNT];
+static OSMesgQueue pi_message_queue;
+
+static OSMesgQueue rdp_message_queue;
+static OSMesg rdp_message_buffer;
+
+static OSMesgQueue retrace_message_queue;
+static OSMesg retrace_message_buffer;
+
+static u16 framebuffers[2][SCREEN_WIDTH * SCREEN_HEIGHT];
+
+// Idle thread. Creates other threads then drops to lowest priority.
+static void idle(void *arg);
+
+// Main game thread.
+static void main(void *arg);
+
+// Main N64 entry point. This must be declared extern.
+void boot(void);
+
+void boot(void) {
+    osInitialize();
+    rom_handle = osCartRomInit();
+    osCreateThread(&idle_thread, 1, idle, NULL, STACK_START(idle_thread_stack),
+                   10);
+    osStartThread(&idle_thread);
+}
+
+static void idle(void *arg) {
+    (void)arg;
+
+    // Initialize video.
+    osCreateViManager(OS_PRIORITY_VIMGR);
+    osViSetMode(&osViModeTable[OS_VI_NTSC_LPN1]);
+
+    // Initialize peripheral manager.
+    osCreatePiManager(OS_PRIORITY_PIMGR, &pi_message_queue, pi_message_buffer,
+                      PI_MSG_COUNT);
+
+    // Start main thread.
+    osCreateThread(&main_thread, 3, main, NULL, STACK_START(main_thread_stack),
+                   10);
+    osStartThread(&main_thread);
+
+    // Idle loop.
+    osSetThreadPri(0, 0);
+    for (;;) {}
+}
+
+// Fill a framebuffer with the given color, whuch must be in 16-bit RGBA format.
+static void fill_framebuffer(u16 (*framebuffer)[SCREEN_WIDTH * SCREEN_HEIGHT],
+                             unsigned color) {
+    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+        (*framebuffer)[i] = color;
+    }
+}
+
+// Create a 16-bit RGBA color from components. Color components must be 5-bit,
+// and the alpha component must be 1-bit.
+static unsigned rgba16(unsigned r, unsigned g, unsigned b, unsigned a) {
+    unsigned c = 0;
+    c |= (r & 31) << 11;
+    c |= (g & 31) << 6;
+    c |= (b & 31) << 1;
+    c |= a & 1;
+    return c;
+}
+
+// Create a fully saturated color from a hue. The hue goes from 0 to 6<<5.
+static unsigned make_hue(unsigned hue) {
+    unsigned r, g, b;
+    switch (hue >> 5) {
+    default:
+    case 0:
+        r = 31;
+        g = hue;
+        b = 0;
+        break;
+    case 1:
+        r = ~hue;
+        g = 31;
+        b = 0;
+        break;
+    case 2:
+        r = 0;
+        g = 31;
+        b = hue;
+        break;
+    case 3:
+        r = 0;
+        g = ~hue;
+        b = 31;
+        break;
+    case 4:
+        r = hue;
+        g = 0;
+        b = 31;
+        break;
+    case 5:
+        r = 31;
+        g = 0;
+        b = ~hue;
+        break;
+    }
+    return rgba16(r, g, b, 0);
+}
+
+static void main(void *arg) {
+    (void)arg;
+
+    // Set up message queues.
+    osCreateMesgQueue(&rdp_message_queue, &rdp_message_buffer, 1);
+    osSetEventMesg(OS_EVENT_DP, &rdp_message_queue, NULL);
+    osCreateMesgQueue(&retrace_message_queue, &retrace_message_buffer, 1);
+    osViSetEvent(&retrace_message_queue, NULL, 1); // 1 = every frame
+
+    int which_framebuffer = 0;
+    unsigned hue = 0;
+
+    for (;;) {
+        unsigned color = make_hue(hue);
+        hue++;
+        if (hue == 6 << 5)
+            hue = 0;
+
+        fill_framebuffer(&framebuffers[which_framebuffer], color);
+        osWritebackDCache(&framebuffers[which_framebuffer],
+                          sizeof(framebuffers[which_framebuffer]));
+
+        osViSwapBuffer(framebuffers[which_framebuffer]);
+        // Remove any old retrace message and wait for a new one.
+        if (MQ_IS_FULL(&retrace_message_queue))
+            osRecvMesg(&retrace_message_queue, NULL, OS_MESG_BLOCK);
+        osRecvMesg(&retrace_message_queue, NULL, OS_MESG_BLOCK);
+        which_framebuffer ^= 1;
+    }
+}
---
+++ sdk/README.md
@@ -0,0 +1,5 @@
+# SDK
+
+The Nintendo 64 SDK must be symlinked here in order to create builds for Nintendo 64.
+
+Create a symlink named "ultra" that points to the "usr" directory inside a Nintendo 64 Developer OS/Library installation. It should be version 2.0L for Windows. The Irix version was made for the IRIX IDO compiler and will not work with GCC.
