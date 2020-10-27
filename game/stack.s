2dea6693b507a294403d023cb82b2dcc469c1b7e
Dietrich Epp <depp@zdome.net>
2020-10-26 22:12:52 -0400

    Move stack definitions to asm file

---
+++ game/stack.s
@@ -0,0 +1,14 @@
+# Stacks for threads and SP tasks.
+	.section .stack
+
+	.global _boot_stack
+	.global _main_thread_stack
+	.space 8 * 1024
+	.align 3
+_boot_stack:
+_main_thread_stack:
+
+	.global _idle_thread_stack
+	.space 8 * 1024
+	.align 3
+_idle_thread_stack:
