ee184f140826f69522f44c8604f426ce9fe4e481
Dietrich Epp <depp@zdome.net>
2020-10-31 17:33:41 -0400

    Add option to run old artifacts

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import argparse
+import json
 import os
 import pathlib
+import re
 import shlex
 import subprocess
 import sys
@@ -71,6 +73,49 @@ def mupen64(rom, args):
     exe = find_program('mupen64plus', extra_paths=EXTRA_PATHS)
     run([exe, rom, *args.emulator_opts])
 
+def find_artifact(revision):
+    dir = pathlib.Path.home() / 'Documents/Artifacts'
+    find_revision = re.compile(r'\.r(\d+)\.')
+    matches = []
+    has_artifact = False
+    for item in dir.iterdir():
+        m = find_revision.search(item.name)
+        if m is not None:
+            has_artifact = True
+            item_rev = int(m.group(1), 10)
+            if item_rev == revision:
+                matches.append(item)
+    if not has_artifact:
+        die('could not find any artifacts in directory:', dir)
+    if not matches:
+        die('could not find artifact with revision:', revision)
+    if len(matches) > 1:
+        die('multiple artifacts with revision:', revision)
+    return matches[0]
+
+def extract_artifact(artifact):
+    temp = SRCDIR / 'temp'
+    temp.mkdir(exist_ok=True)
+    for item in temp.iterdir():
+        item.unlink()
+    subprocess.run(
+        ['tar', 'xf', artifact],
+        cwd=temp,
+        check=True,
+    )
+    with (temp/'INFO.json').open() as fp:
+        info = json.load(fp)
+    print('Revision:', info['revision'])
+    print('Message:')
+    for line in info['message'].splitlines():
+        print('    ' + line.rstrip())
+    roms = [item for item in temp.iterdir() if item.suffix == '.n64']
+    if not roms:
+        die('could not find ROM in artifact')
+    if len(roms) > 1:
+        die('multiple ROMs in artifact')
+    return roms[0]
+
 def main(argv):
     emulators = {
         'cen64': cen64,
@@ -89,9 +134,19 @@ def main(argv):
         help='emulator to run game with')
     p.add_argument('emulator_opts', nargs='*',
         help='additional options to pass emulator')
+    p.add_argument('-artifact',
+        help='run ROM from the given artifact archive')
+    p.add_argument('-revision', '-r', type=int,
+        help='run rom from artifact with the given revision')
     args = p.parse_args(argv)
 
-    rom = pathlib.Path(SRCDIR, 'bazel-bin', 'game', ROM)
+    artifact = args.artifact
+    if artifact is None and args.revision is not None:
+        artifact = find_artifact(args.revision)
+    if artifact is None:
+        rom = pathlib.Path(SRCDIR, 'bazel-bin', 'game', ROM)
+    else:
+        rom = extract_artifact(artifact)
     if args.hardware:
         hardware(rom, args)
     else:
