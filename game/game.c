a95532780b44ed6e328f7ca53b2d06bf5120354c
Dietrich Epp <depp@zdome.net>
2020-12-02 07:25:58 -0500

    Extract texture display list to separate file

--- game/game.c
+++ game/game.c
@@ -13,6 +13,7 @@
 #include "game/defs.h"
 #include "game/graphics.h"
 #include "game/model.h"
+#include "game/texture_n64_dl.h"
 
 #include <stdbool.h>
 
@@ -146,8 +147,6 @@ static const Gfx model_setup_dl[] = {
     gsSPEndDisplayList(),
 };
 
-static Gfx texture_dl[];
-
 static const Gfx fairy_setup_dl[] = {
     gsDPPipeSync(),
     gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, texture[1]),
@@ -171,71 +170,6 @@ void game_update(struct game_state *restrict gs, float dt) {
 static const Lights1 lights =
     gdSPDefLights1(16, 16, 64,                               // Ambient
                    255 - 16, 255 - 16, 255 - 64, 0, 0, 100); // Sun
-
-// Display list to load the ground texture.
-static Gfx texture_dl[] = {
-    gsDPPipeSync(),
-    gsDPSetTexturePersp(G_TP_PERSP),
-    gsDPSetCycleType(G_CYC_2CYCLE),
-    gsDPSetRenderMode(G_RM_PASS, G_RM_ZB_OPA_SURF2),
-    gsSPTexture(0x8000, 0x8000, 5, 0, G_ON),
-    gsDPSetTextureDetail(G_TD_CLAMP),
-    gsDPSetPrimColor(0, 0, 255, 255, 255, 255),
-    gsDPSetTextureLOD(G_TL_LOD),
-    gsDPSetTextureFilter(G_TF_BILERP),
-
-    // Load data into TMEM (using G_TX_LOADTILE).
-
-    gsDPSetTile(       //
-        G_IM_FMT_RGBA, // format
-        G_IM_SIZ_16b,  // size
-        0,             // size of one row
-        0,             // tmem
-        G_TX_LOADTILE, // tile
-        0,             // palette
-        G_TX_NOMIRROR, // cmt
-        0,             // maskt
-        G_TX_NOLOD,    // shiftt
-        G_TX_NOMIRROR, // cms
-        0,             // masks
-        G_TX_NOLOD),   // shifts
-
-    gsDPLoadSync(),
-
-    gsDPLoadBlock(     //
-        G_TX_LOADTILE, // tile
-        0,             // uls
-        0,             // ult
-        1371,          // lrs
-        0),            // dxt
-
-    gsDPPipeSync(),
-
-    // Tile: format, size, length of row in 8-byte units, TMEM address, tile,
-    // palette, cmt, maskt, shiftt, cms, masks, shifts.
-    //
-    // TileSize: tile, uls, ult, lrs, lrt
-    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, 0, 0, 0, 5, 0, 0, 5, 0),
-    gsDPSetTileSize(0, 0, 0, 31 << G_TEXTURE_IMAGE_FRAC,
-                    31 << G_TEXTURE_IMAGE_FRAC),
-    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 4, 256, 1, 0, 0, 4, 1, 0, 4, 1),
-    gsDPSetTileSize(1, 0, 0, 15 << G_TEXTURE_IMAGE_FRAC,
-                    15 << G_TEXTURE_IMAGE_FRAC),
-    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 2, 320, 2, 0, 0, 3, 2, 0, 3, 2),
-    gsDPSetTileSize(2, 0, 0, 7 << G_TEXTURE_IMAGE_FRAC,
-                    7 << G_TEXTURE_IMAGE_FRAC),
-    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, 336, 3, 0, 0, 2, 3, 0, 2, 3),
-    gsDPSetTileSize(3, 0, 0, 3 << G_TEXTURE_IMAGE_FRAC,
-                    3 << G_TEXTURE_IMAGE_FRAC),
-    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, 340, 4, 0, 0, 1, 4, 0, 1, 4),
-    gsDPSetTileSize(4, 0, 0, 1 << G_TEXTURE_IMAGE_FRAC,
-                    1 << G_TEXTURE_IMAGE_FRAC),
-    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, 342, 5, 0, 0, 0, 5, 0, 0, 5),
-    gsDPSetTileSize(5, 0, 0, 0 << G_TEXTURE_IMAGE_FRAC,
-                    0 << G_TEXTURE_IMAGE_FRAC),
-
-    gsSPEndDisplayList(),
-};
 
 // The identity matrix.
 static const Mtx identity = {{
---
+++ game/texture_n64_dl.h
@@ -0,0 +1,7 @@
+// Texture display list definitions.
+#pragma once
+
+#include <ultra64.h>
+
+// 32x32 mipmapped texture display list.
+extern const Gfx texture_dl[];
