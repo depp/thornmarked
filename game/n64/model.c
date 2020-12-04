12a9d43a926b6ef77ef9fdab8e149adcec60c8f4
Dietrich Epp <depp@zdome.net>
2020-12-04 10:00:37 -0500

    Generalize model system

--- game/n64/model.c
+++ game/n64/model.c
@@ -15,6 +15,11 @@
 #include "game/n64/texture.h"
 #include "game/physics.h"
 
+enum {
+    // Number of model assets which can be loaded at once.
+    MODEL_SLOTS = 3,
+};
+
 // A frame in a model animation.
 struct model_frame {
     float time;
@@ -37,11 +42,21 @@ struct model_header {
     struct model_animation animation[];
 };
 
-// Loaded models.
-static union {
+// Model data, including buffer.
+union model_data {
     struct model_header header;
     uint8_t data[200 * 1024];
-} model_data[3] ASSET;
+};
+
+// Loaded models.
+static union model_data model_data[MODEL_SLOTS] ASSET;
+
+// Map from model asset ID to slot number. If the slot maps back to the same
+// model asset, then this texture is loaded, otherwise it is not loaded.
+static int model_to_slot[PAK_MODEL_COUNT + 1];
+
+// Map from model slot number to model asset ID.
+static int model_from_slot[MODEL_SLOTS];
 
 static void *pointer_fixup(void *ptr, uintptr_t base, size_t size) {
     uintptr_t value = (uintptr_t)ptr;
@@ -52,15 +67,18 @@ static void *pointer_fixup(void *ptr, uintptr_t base, size_t size) {
     return (void *)(value + base);
 }
 
-static void model_fixup(struct model_header *restrict p, uintptr_t base,
-                        size_t size) {
-    p->vertex_data = pointer_fixup(p->vertex_data, base, size);
-    p->display_list = pointer_fixup(p->display_list, base, size);
-    for (int i = 0; i < p->animation_count; i++) {
-        struct model_animation *restrict anim = &p->animation[i];
+// Fix the internal pointers in a model after loading.
+static void model_fixup(union model_data *p) {
+    const uintptr_t base = (uintptr_t)p;
+    const size_t size = sizeof(union model_data);
+    struct model_header *restrict hdr = &p->header;
+    hdr->vertex_data = pointer_fixup(hdr->vertex_data, base, size);
+    hdr->display_list = pointer_fixup(hdr->display_list, base, size);
+    for (int i = 0; i < hdr->animation_count; i++) {
+        struct model_animation *restrict anim = &hdr->animation[i];
         if (anim->frame_count > 0) {
             struct model_frame *restrict frame =
-                pointer_fixup(p->animation[i].frame, base, size);
+                pointer_fixup(hdr->animation[i].frame, base, size);
             anim->frame = frame;
             for (int j = 0; j < anim->frame_count; j++) {
                 frame[j].vertex = pointer_fixup(frame[j].vertex, base, size);
@@ -69,17 +87,37 @@ static void model_fixup(struct model_header *restrict p, uintptr_t base,
     }
 }
 
-static void model_load(int slot, pak_model asset_id) {
-    pak_load_asset_sync(model_data[slot].data, sizeof(model_data[slot].data),
-                        PAK_MODEL_START + asset_id.id - 1);
-    struct model_header *p = &model_data[slot].header;
-    model_fixup(p, (uintptr_t)&model_data[slot], sizeof(model_data[slot]));
+// Load a model into the given slot.
+static void model_load_slot(pak_model asset, int slot) {
+    pak_load_asset_sync(&model_data[slot], sizeof(model_data[slot]),
+                        PAK_MODEL_START + asset.id - 1);
+    model_fixup(&model_data[slot]);
+    model_to_slot[asset.id] = slot;
+    model_from_slot[slot] = asset.id;
+}
+
+// Load a model and return the slot.
+static int model_load(pak_model asset) {
+    if (asset.id < 1 || PAK_MODEL_COUNT < asset.id) {
+        fatal_error("model_load: invalid model\nModel: %d", asset.id);
+    }
+    int slot = model_to_slot[asset.id];
+    if (model_from_slot[slot] == asset.id) {
+        return slot;
+    }
+    for (slot = 0; slot < MODEL_SLOTS; slot++) {
+        if (model_from_slot[slot] == 0) {
+            model_load_slot(asset, slot);
+            return slot;
+        }
+    }
+    fatal_error("model_load: no slots available");
 }
 
 void model_render_init(void) {
-    model_load(0, MODEL_FAIRY);
-    model_load(1, MODEL_BLUEENEMY);
-    model_load(2, MODEL_GREENENEMY);
+    model_load(MODEL_FAIRY);
+    model_load(MODEL_BLUEENEMY);
+    model_load(MODEL_GREENENEMY);
 }
 
 static const Gfx fairy_setup_dl[] = {
@@ -94,7 +132,7 @@ static int anim_id, frame_id;
 Gfx *model_render(Gfx *dl, struct graphics *restrict gr,
                   struct sys_model *restrict msys,
                   struct sys_phys *restrict psys) {
-    int current_model = -1, current_model_index = 0;
+    int current_model = 0;
     float scale = 0.5f;
     for (unsigned i = 0; i < psys->count; i++) {
         struct cp_phys *restrict cp = &psys->entities[i];
@@ -106,35 +144,34 @@ Gfx *model_render(Gfx *dl, struct graphics *restrict gr,
             continue;
         }
         int model = mp->model_id.id;
+        int slot = model_to_slot[model];
+        if (model_from_slot[slot] != model) {
+            fatal_error("Model not loaded");
+        }
         if (model != current_model) {
-            int index;
             if (model == MODEL_FAIRY.id) {
-                index = 0;
                 gSPDisplayList(dl++, fairy_setup_dl);
                 dl = texture_use(dl, IMG_FAIRY);
                 gSPSegment(dl++, 1,
-                           K0_TO_PHYS(model_data[index]
+                           K0_TO_PHYS(model_data[slot]
                                           .header.animation[anim_id]
                                           .frame[frame_id]
                                           .vertex));
             } else if (model == MODEL_BLUEENEMY.id) {
-                index = 1;
                 gSPDisplayList(dl++, fairy_setup_dl);
                 dl = texture_use(dl, IMG_BLUEENEMY);
                 gSPSegment(dl++, 1,
-                           K0_TO_PHYS(model_data[index].header.vertex_data));
+                           K0_TO_PHYS(model_data[slot].header.vertex_data));
             } else if (model == MODEL_GREENENEMY.id) {
-                index = 2;
                 gSPDisplayList(dl++, fairy_setup_dl);
                 dl = texture_use(dl, IMG_GREENENEMY);
                 gSPSegment(dl++, 1,
-                           K0_TO_PHYS(model_data[index].header.vertex_data));
+                           K0_TO_PHYS(model_data[slot].header.vertex_data));
             } else {
                 continue;
             }
-            current_model_index = index;
-            current_model = model;
         }
+        current_model = model;
         Mtx *mtx = gr->mtx_ptr++;
         {
             mat4 mat;
@@ -145,9 +182,7 @@ Gfx *model_render(Gfx *dl, struct graphics *restrict gr,
         }
         gSPMatrix(dl++, K0_TO_PHYS(mtx),
                   G_MTX_MODELVIEW | G_MTX_LOAD | G_MTX_NOPUSH);
-        gSPDisplayList(
-            dl++,
-            K0_TO_PHYS(model_data[current_model_index].header.display_list));
+        gSPDisplayList(dl++, K0_TO_PHYS(model_data[slot].header.display_list));
     }
 
     frame_id++;
