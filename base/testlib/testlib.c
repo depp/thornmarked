c605d0e3daec4a696e05e4e85a82f1fef5332db6
Dietrich Epp <depp@zdome.net>
2020-12-05 13:21:08 -0500

    Fix testlib on N64

--- base/testlib/testlib.c
+++ base/testlib/testlib.c
@@ -9,7 +9,9 @@
 #if _ULTRA64
 
 #include "base/console.h"
-#include "base/console_n64.h"
+#include "base/n64/console.h"
+#include "base/n64/os.h"
+#include "base/n64/system.h"
 
 #include <ultra64.h>
 
