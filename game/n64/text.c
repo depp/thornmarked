83586ba10078c8a1f65b27bcb9f41255d62c4ec4
Dietrich Epp <depp@zdome.net>
2020-12-06 18:20:15 -0500

    Redesign font format
    The previous font code had some parsing bugs involving multiple
    textures, which weren't tested.

--- game/n64/text.c
+++ game/n64/text.c
@@ -2,22 +2,16 @@
 
 #include "assets/pak.h"
 #include "base/base.h"
+#include "base/fixup.h"
 #include "base/pak/pak.h"
 
 #include <ultra64.h>
 
 #include <stdint.h>
 
-struct font_header {
-    uint16_t charmap_size;
-    uint16_t glyph_count;
-    uint16_t texture_count;
-};
-
-struct font_char {
-    uint16_t codepoint;
-    uint16_t glyph;
-};
+// =============================================================================
+// Font Loading
+// =============================================================================
 
 struct font_glyph {
     uint8_t size[2];
@@ -27,10 +21,20 @@ struct font_glyph {
     uint8_t advance;
 };
 
+struct font_header {
+    uint16_t glyph_count;
+    uint16_t texture_count;
+    struct font_texture *textures;
+    uint8_t charmap[256];
+    struct font_glyph glyphs[];
+};
+
 struct font_texture {
     uint16_t width;
     uint16_t height;
-    uint8_t pixels[];
+    uint16_t pix_fmt;
+    uint16_t pix_size;
+    void *pixels;
 };
 
 union font_buffer {
@@ -38,51 +42,44 @@ union font_buffer {
     uint8_t data[8 * 1024];
 };
 
-static union font_buffer font_buffer __attribute__((aligned(16)));
-static struct font_char *charmap;
-static struct font_glyph *glyphinfo;
-static struct font_texture *textures[4];
+// Fix internal pointers in font after loading.
+static void font_fixup(union font_buffer *p) {
+    const uintptr_t base = (uintptr_t)p;
+    const size_t size = sizeof(union font_buffer);
+    struct font_header *restrict hdr = &p->header;
+    hdr->textures = pointer_fixup(hdr->textures, base, size);
+    for (int i = 0; i < hdr->texture_count; i++) {
+        struct font_texture *restrict tex = &hdr->textures[i];
+        tex->pixels = pointer_fixup(tex->pixels, base, size);
+    }
+}
+
+static union font_buffer font_buffer ASSET;
 
 void font_load(pak_data asset_id) {
     // Load from cartridge memory.
     pak_load_asset_sync(font_buffer.data, sizeof(font_buffer.data),
                         pak_data_object(asset_id));
-
-    // Get pointers to all the objects inside.
-    const struct font_header *restrict h = &font_buffer.header;
-    const uint8_t *restrict ptr = font_buffer.data + 8;
-    charmap = (void *)ptr;
-    ptr += sizeof(struct font_char) * h->charmap_size;
-    glyphinfo = (void *)ptr;
-    ptr += sizeof(struct font_glyph) * h->glyph_count;
-    for (int i = 0; i < h->texture_count; i++) {
-        struct font_texture *tex = (void *)ptr;
-        textures[i] = tex;
-        uint32_t size =
-            sizeof(struct font_texture) + (tex->width >> 1) * tex->height;
-        size = (size + 3) & ~3; // FIXME: macro / function for aligning.
-        ptr += size;
-    }
+    font_fixup(&font_buffer);
 }
+
+// =============================================================================
+// Text Rendering
+// =============================================================================
 
 static uint16_t glyphs[256];
 
-static uint16_t *text_to_glyphs(uint16_t *gptr, const char *text) {
-    const struct font_char *cstart = charmap,
-                           *cend = charmap + font_buffer.header.charmap_size;
+static uint16_t *text_to_glyphs(const struct font_header *restrict fn,
+                                uint16_t *gptr, const char *text) {
     while (*text != '\0') {
         unsigned cp = (unsigned char)*text++;
-        for (const struct font_char *cptr = cstart; cptr != cend; cptr++) {
-            if (cptr->codepoint == cp) {
-                *gptr++ = cptr->glyph;
-                break;
-            }
-        }
+        *gptr++ = fn->charmap[cp];
     }
     return gptr;
 }
 
 Gfx *text_render(Gfx *dl, Gfx *dl_end, int x, int y, const char *text) {
+    const struct font_header *restrict fn = &font_buffer.header;
     if (16 > dl_end - dl) {
         fatal_dloverflow();
     }
@@ -93,12 +90,26 @@ Gfx *text_render(Gfx *dl, Gfx *dl_end, int x, int y, const char *text) {
     gDPSetPrimColor(dl++, 0, 0, 255, 128, 0, 255);
     gDPSetRenderMode(dl++, G_RM_XLU_SURF, G_RM_XLU_SURF2);
     gDPSetCombineMode(dl++, G_CC_MODULATEIA_PRIM, G_CC_MODULATEIA_PRIM);
-    struct font_texture *tex = textures[0];
-    gDPLoadTextureBlock_4b(dl++, tex->pixels, G_IM_FMT_I, tex->width,
-                           tex->height, 0, 0, 0, 0, 0, 0, 0);
-    uint16_t *gend = text_to_glyphs(glyphs, text);
+    struct font_texture *tex = &fn->textures[0];
+    switch (tex->pix_size) {
+    case G_IM_SIZ_4b:
+        gDPLoadTextureBlock_4b(dl++, tex->pixels, tex->pix_fmt, tex->width,
+                               tex->height, 0, 0, 0, 0, 0, 0, 0);
+        break;
+    case G_IM_SIZ_8b:
+        gDPLoadTextureBlock(dl++, tex->pixels, tex->pix_fmt, G_IM_SIZ_8b,
+                            tex->width, tex->height, 0, 0, 0, 0, 0, 0, 0);
+        break;
+    case G_IM_SIZ_16b:
+        gDPLoadTextureBlock(dl++, tex->pixels, tex->pix_fmt, G_IM_SIZ_16b,
+                            tex->width, tex->height, 0, 0, 0, 0, 0, 0, 0);
+        break;
+    default:
+        fatal_error("Unsupported font texture size\nSize: %d", tex->pix_size);
+    }
+    uint16_t *gend = text_to_glyphs(fn, glyphs, text);
     for (uint16_t *gptr = glyphs; gptr != gend; gptr++) {
-        struct font_glyph *restrict gi = glyphinfo + *gptr;
+        const struct font_glyph *restrict gi = &fn->glyphs[*gptr];
         if (gi->size[0] != 0) {
             if (3 > dl_end - dl) {
                 fatal_dloverflow();
