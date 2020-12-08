5ad2e36a8e4abd49cf97c54298f1dd2e2d367adc
Dietrich Epp <depp@zdome.net>
2020-12-08 01:10:58 -0500

    Fix incorrect data in VADPCM loop chunk

--- base/xmalloc.c
+++ base/xmalloc.c
@@ -12,3 +12,14 @@ void *xmalloc(size_t size) {
     }
     return ptr;
 }
+
+void *xcalloc(size_t nmemb, size_t size) {
+    if (nmemb == 0 || size == 0) {
+        return NULL;
+    }
+    void *ptr = calloc(nmemb, size);
+    if (ptr == NULL) {
+        die("calloc failed: %zu * %zu bytes", nmemb, size);
+    }
+    return ptr;
+}
