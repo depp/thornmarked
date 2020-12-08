8e1ae0e95fa26fe39d553c06cba0d0e8e3b9d16f
Dietrich Epp <depp@zdome.net>
2020-12-08 01:32:39 -0500

    Add NULL check during pointer fixup

--- base/fixup.h
+++ base/fixup.h
@@ -10,6 +10,9 @@ noreturn void pointer_fixup_error(void *ptr, uintptr_t base, size_t size);
 // offet is smaller than the given size.
 inline void *pointer_fixup(void *ptr, uintptr_t base, size_t size) {
     uintptr_t value = (uintptr_t)ptr;
+    if (value == 0) {
+        return NULL;
+    }
     if (value > size) {
         pointer_fixup_error(ptr, base, size);
     }
