956ff6c35d7106565b7292c69692252a7f105d5e
Dietrich Epp <depp@zdome.net>
2020-12-11 01:46:39 -0500

    Calculate TMEM addresses with texsize.py

--- scripts/texsize.py
+++ scripts/texsize.py
@@ -6,19 +6,21 @@ def die(*msg):
 
 def main(argv):
     try:
-        xs, ys = argv
+        bitss, xs, ys = argv
     except ValueError:
-        die('Usage: texsize.py <width> <height>')
+        die('Usage: texsize.py <bits> <width> <height>')
+    bits = int(bitss)
     x = int(xs)
     y = int(ys)
     size = 0
     while x and y:
-        stride = ((x + 15) & ~15) >> 1
+        print('{:4}    {}x{}'.format(size, x, y))
+        stride = (x * bits + 63) >> 6
         size += stride * y
-        print('{:04}    {}x{}'.format(size, x, y))
         x >>= 1
         y >>= 1
-    print('{:04}'.format(size))
+    print('{:4}'.format(size))
+    print('Bytes:', size << 3)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
